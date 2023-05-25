e4ee5115c3daaae8db3cdf7a2b93afd96862551b
David <davidmejia.05@hotmail.com>
2023-05-25 12:58:27 -0600

    check

--- src/components/projects/ProjectCards.jsx
+++ src/components/projects/ProjectCards.jsx
@@ -1,27 +1,34 @@
-import {Col} from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({project}) => {
-    console.log(project)
+export const ProjectCard = ({ project }) => {
+  //    console.log(project)
   return (
     <Col sm={6} md={4}>
-      <div className="project-text">
-        <h3>{project.name} <span className="project-text-company">for {project.company}</span></h3>
-        <p1>{project.period}<br/>{project.description}</p1>
+      <div className="proj-imgbx">
+        <h3>
+          {project.name}{" "}
+          <span className="">
+            for {project.company.name}
+          </span>
+        </h3>
+        <p1>
+          {project.period}
+          <br />
+          {project.description}
+        </p1>
         
-        <ul>
-        {project.activities.map((activity, index) => {
-            return(
-                <li>{activity}</li>
-            )
-        })}
+        <ul className="proj-txtx">
+          {project.activities.map((activity, index) => {
+            return <li>{activity}</li>;
+          })}
         </ul>
 
         <div>
-      {project.stack.map((item, index) => (
-        <span key={index}> * {item}  </span>
-      ))} *
-    </div>
-
+          {project.stack.map((item, index) => (
+            <span key={index}> * {item} </span>
+          ))}{" "}
+          *
+        </div>
       </div>
     </Col>
   );
