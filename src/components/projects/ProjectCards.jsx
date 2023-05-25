65b2bef7dda1fc436ad548622494f64e1eb56e4d
David <davidmejia.05@hotmail.com>
2023-05-25 14:49:52 -0600

    fix cards

--- src/components/projects/ProjectCards.jsx
+++ src/components/projects/ProjectCards.jsx
@@ -5,30 +5,31 @@ export const ProjectCard = ({ project }) => {
   return (
     <Col sm={6} md={4}>
       <div className="proj-imgbx">
-        <h3>
-          {project.name}{" "}
-          <span className="">
-            for {project.company.name}
-          </span>
-        </h3>
-        <p1>
-          {project.period}
-          <br />
-          {project.description}
-        </p1>
-        
+        <div className="proj-bx-txt">
+          {" "}
+          <h3>
+            {project.name} <span className="">for {project.company.name}</span>
+          </h3>
+          <p1>
+            Period: {project.period}
+            <br />
+            Description: {project.description}
+            <br />
+            Stack: 
+            {project.stack.map((item, index) => (
+              <span key={index}> {item},</span>
+            ))}{" "}
+          </p1>
+        </div>
+
+        <div></div>
+
         <ul className="proj-txtx">
+          <h3>Activities</h3>
           {project.activities.map((activity, index) => {
             return <li>{activity}</li>;
           })}
         </ul>
-
-        <div>
-          {project.stack.map((item, index) => (
-            <span key={index}> * {item} </span>
-          ))}{" "}
-          *
-        </div>
       </div>
     </Col>
   );
