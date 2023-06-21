236c3411b9e6ee17957f047cebcf44c799ef62ab
David <dmg_92@hotmail.com>
2023-06-21 14:30:15 -0600

    skills

--- src/components/projects/ProjectCards.jsx
+++ src/components/projects/ProjectCards.jsx
@@ -1,5 +1,6 @@
 import { Col } from "react-bootstrap";
 import Accordion from "react-bootstrap/Accordion";
+import { CustomIcon } from "../icons/Icons";
 
 export const ProjectCard = ({ index, project }) => {
   return (
@@ -9,15 +10,17 @@ export const ProjectCard = ({ index, project }) => {
           <Accordion.Header>
               {project.name} <br />
               {project.period}
-            
+            <br/>
+            <p>{project.stack.map((skill, index) => (
+              <CustomIcon name={skill.icon? skill.icon: "Si"+ skill.name} color={skill.color}/> 
+                
+            ))}</p>
           </Accordion.Header>
           <Accordion.Body>
             <strong>Description:</strong> {project.description}
             <br />
             <strong>Stack:</strong>
-            {project.stack.map((item, index2) => (
-              <span key={index2}> {item},</span>
-            ))}{" "}
+
             <ul className="proj-txtx">
               <h3>Activities</h3>
               {project.activities.map((activity, index3) => {
