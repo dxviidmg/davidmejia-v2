08a4fe913ae1bb826b6b91f196f6006bcfa3f75f
David <dmg_92@hotmail.com>
2023-08-14 16:32:38 -0600

    tab container

---
+++ src/components/commons/tabContainer/TabContainer.jsx
@@ -0,0 +1,31 @@
+import { Row, Nav, Tab } from "react-bootstrap";
+
+export const TabContainer = ({ classifications, contents }) => {
+  return (
+    <Tab.Container id="contents-tabs" defaultActiveKey="0">
+      <Nav
+        variant="pills"
+        className="nav-pills justify-content-center align-items-center"
+        id="contents-tab"
+      >
+        {classifications.map((classification, index) => {
+          return (
+            <Nav.Item>
+              <Nav.Link eventKey={index}>{classification}</Nav.Link>
+            </Nav.Item>
+          );
+        })}
+      </Nav>
+
+      <Tab.Content>
+        {contents.map((content, index) => {
+          return (
+            <Tab.Pane eventKey={index}>
+              <Row>{content}</Row>
+            </Tab.Pane>
+          );
+        })}
+      </Tab.Content>
+    </Tab.Container>
+  );
+};
--- src/components/sections/skills/Skills.jsx
+++ src/components/sections/skills/Skills.jsx
@@ -1,6 +1,7 @@
 import { SkillListsByCategories } from "./byCategories/SkillsByCategories";
 import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
 import { SkillsByExperience } from "./byExperience/SkillsByExperience";
+import { TabContainer } from "../../commons/tabContainer/TabContainer";
 
 export const Skills = () => {
   return (
@@ -9,32 +10,7 @@ export const Skills = () => {
         <Row>
           <Col className="text-center">
             <h2>Skills</h2>
-            <Tab.Container id="skills-tabs" defaultActiveKey="first">
-              <Nav
-                variant="pills"
-                className="nav-pills justify-content-center align-items-center"
-                id="skill-tab"
-              >
-                <Nav.Item>
-                  <Nav.Link eventKey="first">By category</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">By expertis</Nav.Link>
-                </Nav.Item>
-              </Nav>
-              <Tab.Content>
-                <Tab.Pane eventKey="first">
-                  <Row>
-                    <SkillListsByCategories />
-                  </Row>
-                </Tab.Pane>
-
-                <Tab.Pane eventKey="second">
-                  <SkillsByExperience></SkillsByExperience>
-                  <Row></Row>
-                </Tab.Pane>
-              </Tab.Content>
-            </Tab.Container>
+            <TabContainer classifications = {["By Category", "By expertis"]} contents={[<SkillListsByCategories/>, <SkillsByExperience/>]} />
           </Col>
         </Row>
       </Container>
