bec8d1369478c7cf4abfba28f75d075137a8a2ff
David <dmg_92@hotmail.com>
2023-09-04 11:16:58 -0600

    x

--- src/components/sections/about/Photo.jsx
+++ src/components/sections/about/Photo.jsx
@@ -4,8 +4,6 @@ import img1 from "./../../../assets/Foto.jpeg"
 
 export const Photo = () => {
     const [isHovered, setIsHovered] = useState(false);
-    const img2 = "https://scontent.fmex30-1.fna.fbcdn.net/v/t39.30808-6/271182417_4802930113102012_1349076978317061846_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_eui2=AeEyf7EBLHKUlrfPX4aOtZjdTi7H4B9w5MVOLsfgH3DkxbF0-3S9wknseZUxkL2zT6Y-HAiry2zpqj646Inpne-L&_nc_ohc=Ir5SsFHew8kAX-j4rjP&_nc_ht=scontent.fmex30-1.fna&oh=00_AfBL8Qm4Hioj0smIn3vBOL_I9ht9TIoNe23DuOR2GnDPhQ&oe=64CDAA76";
-
   
     const handleHover = () => {
       setIsHovered(!isHovered);
