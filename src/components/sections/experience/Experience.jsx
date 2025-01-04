a30a2043caea8872ae7d4ac8444e6e1ed94d358f
David <dmg_92@hotmail.com>
2025-01-04 00:59:25 -0600

    paises

--- src/components/sections/experience/Experience.jsx
+++ src/components/sections/experience/Experience.jsx
@@ -10,10 +10,15 @@ export const Experience = () => {
         <Row>
           {positions.map((position, index) => {
             const positionInfo = `${position.position} at ${position.company.name}`;
+
+            const country = position.location.includes("Boston")
+              ? "https://upload.wikimedia.org/wikipedia/commons/8/88/United-states_flag_icon_round.svg"
+              : "https://upload.wikimedia.org/wikipedia/commons/c/c0/Mexico_flag_icon.svg";
             return (
               <Col sm={12} md={6} key={index}>
                 <h3>
-                  {position.position} {positionInfo.length > 55 ? "" : (<br></br>)} at{" "}
+                  {position.position}{" "}
+                  {positionInfo.length > 55 ? "" : <br></br>} at{" "}
                   <a
                     href={position.company.url}
                     target="_blank"
@@ -23,22 +28,29 @@ export const Experience = () => {
                   </a>
                 </h3>
                 <Row>
-                  <Col sm={12} md={8}>
+                  <Col sm={12} md={6}>
                     <p>
                       <strong>Period:</strong> {position.period}
                       <br />
-                      <strong>Location:</strong> {position.location}
-                      <br />
                       <strong>Modality:</strong> {position.modality}
                       <br />
-                      {position.notes}
+                      <strong>Location:</strong> {position.location}{" "}
+                      <span>
+                        {" "}
+                        <Image
+                          src={country}
+                          roundedCircle
+                          style={{ height: "22px", paddingBottom: '3px' }}
+                        />
+                      </span>
+                      <br />
                     </p>
                   </Col>
-                  <Col sm={12} md={3}>
+                  <Col sm={12} md={6} className="">
                     <Image
                       src={position.logo}
                       roundedCircle
-                      style={{ height: "100px" }}
+                      style={{ height: "70px" }}
                       thumbnail
                     />
                   </Col>
