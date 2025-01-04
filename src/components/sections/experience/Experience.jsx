28849d45b48d3e801a8a37d91dc35c2427718f75
David <dmg_92@hotmail.com>
2025-01-03 23:26:10 -0600

    exp proto

--- src/components/sections/experience/Experience.jsx
+++ src/components/sections/experience/Experience.jsx
@@ -1,7 +1,6 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
 import positions from "../../../data/positions.json";
-import './experience.css'
-
+import "./experience.css";
 
 export const Expirience = () => {
   return (
@@ -11,26 +10,59 @@ export const Expirience = () => {
 
         <Row>
           {positions.map((position, index) => {
+
+            const position_length = position.position +  " at " + position.company.name
             return (
-              <Col sm={12} md={6} key={index}>
-                {" "}
-                <h3>
-                  {position.position} at{" "}
-                  <a
-                    href={position.company.url}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {position.company.name}
-                  </a>
-                </h3>
-                <p>
-                  <strong>Period:</strong> {position.period}. <br />
-                  <strong>Location:</strong> {position.location}. <br />{" "}
-                  <strong>Modality:</strong> {position.modality} <br />{" "}
-                  {position.notes}
-                </p>
-              </Col>
+              <>
+                <Col sm={12} md={6} key={index}>
+                  {" "}
+
+                  {position_length.length}
+                  {position_length.length > 40 ? (
+                    <h4>
+                      {position.position}  at{" "}
+                      <a
+                        href={position.company.url}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {position.company.name}
+                      </a>
+                    </h4>
+                  ) : (
+                    <h3>
+                      {position.position} at{" "}
+                      <a
+                        href={position.company.url}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {position.company.name}
+                      </a>
+                    </h3>
+                  )}
+                  <Row>
+                    <Col sm={12} md={6} key={index}>
+                      {" "}
+                      <p>
+                        <strong>Period:</strong> {position.period}. <br />
+                        <strong>Location:</strong> {position.location}. <br />{" "}
+                        <strong>Modality:</strong> {position.modality} <br />{" "}
+                        {position.notes}
+                      </p>
+                    </Col>
+                    <Col sm={12} md={6} key={index}>
+                      {" "}
+                      <Image
+                        src={position.logo}
+                        roundedCircle
+                        style={{ height: "100px" }}
+                        thumbnail
+                      ></Image>
+                    </Col>
+                  </Row>
+                </Col>
+              </>
             );
           })}
         </Row>
