d3e410e9ab6639ca141ae23a6d7717c798651e50
David <dmg_92@hotmail.com>
2026-03-14 16:43:38 -0600

    feat: add WhatsApp floating button for direct contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { LangProvider } from './utils/LangContext';
+import { FaWhatsapp } from 'react-icons/fa';
 import { Banner } from './components/sections/banner/Banner';
 import { Skills } from './components/sections/skills/Skills';
 import { Projects } from './components/sections/projects/Projects';
@@ -21,6 +22,15 @@ function App() {
         <Projects />
         <Certifications />
         <Footer />
+        <a
+          href="https://wa.me/525561652599"
+          target="_blank"
+          rel="noreferrer"
+          className="whatsapp-float"
+          aria-label="Chat on WhatsApp"
+        >
+          <FaWhatsapp />
+        </a>
       </div>
     </LangProvider>
   );
